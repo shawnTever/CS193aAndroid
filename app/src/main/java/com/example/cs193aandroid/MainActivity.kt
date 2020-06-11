19226e97050432bf9c97291e591e9e1f6f77d383
Shawn <shawntimmel@gmail.com>
2020-06-11 22:28:03 +0800

    widgets

--- app/src/main/java/com/example/cs193aandroid/MainActivity.kt
+++ app/src/main/java/com/example/cs193aandroid/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.cs193aandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+//    when equals if
+    fun radioButtonClick(view: View){
+        val id = when (view) {
+            radioButtonA -> R.drawable.a
+            radioButtonB -> R.drawable.b
+            radioButtonC -> R.drawable.c
+            else -> R.drawable.d
+        }
+        block.setImageResource(id)
+//        when {
+//            view.id == R.id.radioButtonA -> {
+//                block.setImageResource(R.drawable.a)
+//            }
+//            view == radioButtonB -> {
+//                block.setImageResource(R.drawable.b)
+//            }
+//            view == radioButtonC -> {
+//                block.setImageResource(R.drawable.c)
+//            }
+//            view == radioButtonD -> {
+//                block.setImageResource(R.drawable.d)
+//            }
+//        }
     }
 }
