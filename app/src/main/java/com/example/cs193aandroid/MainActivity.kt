dcff15e158622bd10928f7a1cb7ce8acbd524adb
Shawn <shawntimmel@gmail.com>
2020-06-12 17:13:09 +0800

    Lists

--- app/src/main/java/com/example/cs193aandroid/MainActivity.kt
+++ app/src/main/java/com/example/cs193aandroid/MainActivity.kt
@@ -3,12 +3,24 @@ package com.example.cs193aandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        blocklist.setOnItemClickListener { list, item, index, id ->
+//            Toast.makeText(this, "you clicked item $index with id $id !", Toast.LENGTH_SHORT).show()
+            val id = when (index) {
+                0 -> R.drawable.a
+                1 -> R.drawable.b
+                2 -> R.drawable.c
+                else -> R.drawable.d
+            }
+            block.setImageResource(id)
+        }
     }
 
 //    when equals if
