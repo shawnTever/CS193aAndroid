2e5f176fff8ea5970132d8d427b3b352351b25c4
Shawn <shawntimmel@gmail.com>
2020-07-02 23:19:33 +0800

    FlagsActivity

--- app/src/main/java/com/example/cs193aandroid/FlagsActivity.kt
+++ app/src/main/java/com/example/cs193aandroid/FlagsActivity.kt
@@ -4,6 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class FlagsActivity : AppCompatActivity() {
+    private val COUNTRIES = listOf("Australia",
+        "Brazil",
+        "China",
+        "Egypt",
+        "France",
+        "Germany",
+        "Ghana",
+        "Italy",
+        "Japan",
+        "Mexico",
+        "Nepal",
+        "Spain",
+        "United Kingdom",
+        "United States")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flags)
