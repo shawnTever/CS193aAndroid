264ddd0de9c24d2991addd07ccc434fa85b50a44
Shawn <shawntimmel@gmail.com>
2020-07-06 01:29:08 +0800

    library yoyo

--- app/src/main/java/com/example/cs193aandroid/LibraryActivity.kt
+++ app/src/main/java/com/example/cs193aandroid/LibraryActivity.kt
@@ -3,23 +3,35 @@ package com.example.cs193aandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_library.*
 
 class LibraryActivity : AppCompatActivity() {
+    companion object{
+        val GLOBALPARA = ""
+//    LibraryActivity.GLOBALPARA
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
-    }
-
-    fun onclickShow(view: View) {
-        val pups = resources.getStringArray(R.array.addressArray)
-        val index = spinner.selectedItemPosition
-        val pup = pups[index]
-
+        
+//        Library Picasso
         Picasso.get()
             .load(R.drawable.a)
             .into(photo_image)
+        Thread.sleep(200)
+    }
 
+    fun onclickShow(view: View) {
+//        val pups = resources.getStringArray(R.array.addressArray)
+//        val index = spinner.selectedItemPosition
+//        val pup = pups[index]
+
+//        Library yoyo
+        YoYo.with(Techniques.Wobble)
+            .duration(1000)
+            .playOn(button)
     }
 }
